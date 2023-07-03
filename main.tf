372856b3e163f8efbc92d679b8c8dbb933196ac9
preeti_kaur <preeti_kaur@persistent.com>
2023-07-03 15:10:25 +0530

    my first commit

---
+++ main.tf
@@ -0,0 +1,38 @@
+
+
+provider "aws" {
+  access_key = "your aws access key here"
+  secret_key = "your aws secret key here"
+  region     = "ap-south-1"
+}
+
+
+resource "aws_instance" "ourfirst" {
+  ami           = "ami-0447a12f28fddb066"
+  instance_type = "t2.micro"01~OAOAOAOAODODOD
+
+:wq
+
+
+
+
+
+
+provider "gcp" {
+  region     = "us-south-1"
+}
+
+
+resource "compute instance"  {
+  instance_type = "EC2"
+  }[200~provider "gcp" {
+  region     = "us-south-1"
+}
+
+
+resource "compute instance"  {
+  instance_type = "EC2"
+  }
+:qa
+
+
